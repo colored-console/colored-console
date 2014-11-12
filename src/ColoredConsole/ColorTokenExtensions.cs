49daab690905b8eaf6ec82e93be1a136b8d76634
Dave Timmins <dwtimmins@outlook.com>
2014-11-12 14:44:13 +1300

    Added foreground colortoken extensions #17

--- src/ColoredConsole/ColorTokenExtensions.cs
+++ src/ColoredConsole/ColorTokenExtensions.cs
@@ -101,5 +101,95 @@ namespace ColoredConsole
         {
             return token.BackgroundColor(ConsoleColor.Yellow);
         }
+
+        [SuppressMessage(
+            "Microsoft.Naming",
+            "CA1719:ParameterNamesShouldNotMatchMemberNames",
+            MessageId = "1#",
+            Justification = "By design.")]
+        public static ColorToken Color(this ColorToken token, ConsoleColor? color)
+        {
+            return new ColorToken(token.Text, color, token.BackgroundColor);
+        }
+
+        public static ColorToken Black(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Black);
+        }
+
+        public static ColorToken Blue(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Blue);
+        }
+
+        public static ColorToken Cyan(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Cyan);
+        }
+
+        public static ColorToken DarkBlue(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkBlue);
+        }
+
+        public static ColorToken DarkCyan(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkCyan);
+        }
+
+        public static ColorToken DarkGray(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkGray);
+        }
+
+        public static ColorToken DarkGreen(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkGreen);
+        }
+
+        public static ColorToken DarkMagenta(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkMagenta);
+        }
+
+        public static ColorToken DarkRed(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkRed);
+        }
+
+        public static ColorToken DarkYellow(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.DarkYellow);
+        }
+
+        public static ColorToken Gray(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Gray);
+        }
+
+        public static ColorToken Green(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Green);
+        }
+
+        public static ColorToken Magenta(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Magenta);
+        }
+
+        public static ColorToken Red(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Red);
+        }
+
+        public static ColorToken White(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.White);
+        }
+
+        public static ColorToken Yellow(this ColorToken token)
+        {
+            return token.Color(ConsoleColor.Yellow);
+        }
     }
 }
